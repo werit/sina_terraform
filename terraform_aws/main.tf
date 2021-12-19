81427d1309c0c2e880689a9192c78caed61b8c3e
tibor-ec2 <tib.balaz@gmail.com>
2021-12-19 16:26:36 +0000

    added nodered image with some basic outputs

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -1,12 +1,33 @@
 terraform {
-    required_providers {
-      docker = {
-        source  = "kreuzwerker/docker"
-        version = "2.15.0"
-      }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
     }
   }
+}
 
-  provider "docker" {
-    
+provider "docker" {
+
+}
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+resource "docker_container" "nodered_container" {
+  name  = "first_node_container"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
   }
+}
+
+output "Ip-address" {
+  value = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  description = "The ip address and port of docker container"
+}
+
+output "Container-name" {
+  value = docker_container.nodered_container.name
+  description = "Name of docker container"
+}
